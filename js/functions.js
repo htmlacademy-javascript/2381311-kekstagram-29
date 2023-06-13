d0cca3f05edfcdaf0a8b9e348b29ea61e435c62b
leksiru <sss.golubeva2@icloud.com>
2023-06-13 23:22:20 +0300

    Созданы функции

---
+++ js/functions.js
@@ -0,0 +1,25 @@
+//первая функция
+const isStringValid = (string, maxLength) => string.length <= maxLength;
+
+isStringValid('vftgfrhdjk');
+
+
+//вторая функция
+function isPalindrom (rawString) {
+  const cleanedStr = rawString.replaceAll(' ', '').toLowerCase();
+  const reversedStr = cleanedStr.split('').reverse().join('');
+
+  return cleanedStr === reversedStr;
+}
+
+
+//3-я функция
+function extractNumber (str) {
+  const extracted = str.toString().replace(/\D/g, '');
+
+  if (extracted === '') {
+    return NaN;
+  }
+
+  return parseInt(extracted, 10);
+}
