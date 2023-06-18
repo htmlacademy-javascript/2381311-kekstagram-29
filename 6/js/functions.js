bcba02adc5aecf444a225647d777839eb0ff056f
Keks <keks@htmlacademy.ru>
2023-06-19 02:25:38 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/functions.js
@@ -0,0 +1,43 @@
+//первая функция
+const isStringValid = (string, maxLength) => string.length <= maxLength;
+
+isStringValid('vftgfrhdjk');
+
+/*
+//вторая функция
+function isPalindrom (rawString) {
+  const cleanedStr = rawString.replaceAll(' ', '').toLowerCase();
+  const reversedStr = cleanedStr.split('').reverse().join('');
+
+  return cleanedStr === reversedStr;
+}
+
+isPalindrom();
+*/
+
+//вторая функция (учитывая рекомендации с созвона)
+function isPalindrom (rawString) {
+  let left = 0;
+  let right = rawString.length - 1;
+  while (left <= right) {
+    if (rawString[left] !== rawString[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
+
+console.log(isPalindrom(dfgfd));
+
+//3-я функция
+function extractNumber (str) {
+  const extracted = str.toString().replace(/\D/g, '');
+
+  if (extracted === '') {
+    return NaN;
+  }
+
+  return parseInt(extracted, 10);
+}
+
+extractNumber();
