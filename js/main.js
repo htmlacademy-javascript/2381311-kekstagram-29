83e48fa535539f56ddf236f7bda98a8ca9519d5e
leksiru <sss.golubeva2@icloud.com>
2023-08-07 23:07:26 +0300

    меняет модули data.js, main.js и util.js; добавляет модули gallery.js и fullSizePhoto.js. Выполнено задание 8.1. Возникает ошибка closePictureEvt, необходимо решить

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 import { createPhoto } from './data.js';
-import { renderPictures } from './thumbnail.js';
 
-import {} from './fullSizePhoto.js';
-
+import { renderThumbnails } from './thumbnail.js';
 // Создаем массив с данными фотографий
-const photoss = Array.from({ length: 25 }, createPhoto);
+const getPhotos = Array.from({ length: 25 }, createPhoto);
 
 // Отрисовываем миниатюры фотографий
-renderPictures(photoss);
+renderThumbnails(getPhotos());
--- js/util.js
+++ js/util.js
@@ -3,7 +3,7 @@
 function getRandomInt (min, max) {
   const lower = Math.ceil(Math.min(min, max));
   const upper = Math.floor(Math.max(min, max));
-  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+  return Math.floor(Math.random() * (upper - lower + 1) + lower);
 }
 
 //2. создать функцию, которая будет генерировать уникальный идентификатор
